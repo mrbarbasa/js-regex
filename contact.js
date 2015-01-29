585bd581aa2fa77b1fc31c400d715885fb90b602
Marifel <mrbarbasa@gmail.com>
2015-01-28 21:46:34 -1000

    Pass emails and phone numbers tests

--- contact.js
+++ contact.js
@@ -6,6 +6,8 @@ module.exports = {
    * @return {[String]} an array of emails, or an empty array if none are found.
    */
   findEmails: function(phrase) {
+    var matches = phrase.match(/\b\w+@\w+.\w+\b/g);
+    return matches ? matches : [];
   },
 
   /**
@@ -14,6 +16,8 @@ module.exports = {
    * @return {[String]]} an array of phone numbers.
    */
   findPhoneNumbers: function(phrase) {
+    var matches = phrase.match(/(\d{10})|(\d{3}-\d{3}-\d{4})|(\(\d{3}\)\s\d{3}-\d{4})/g);
+    return matches ? matches : [];
   }
 
 };
